8da7dcbcda2a069915db649e3cf1d1ec13e677d2
Vivien Wezel <v.wezel@dotsource.de>
2024-05-17 17:58:16 +0200

    typescript fucked

---
+++ server/api/droppedShows.ts
@@ -0,0 +1,15 @@
+export default defineEventHandler((event) => {
+    const config = useRuntimeConfig(event)
+    const url = `${config.public.apiBase}/list/8296910`;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${config.apiSecret}`
+        }
+    };
+
+    return fetch(url, options)
+        .then(res => res.json())
+        .catch(err => console.error('error:' + err));
+})
--- vue components/SeriesPage.vue
+++ vue components/SeriesPage.vue
@@ -1,3 +1,36 @@
 <template>
-test tes testsokad
+    <div class="series-tile" v-for="item in watchedData?.items">
+        {{item.original_name}}
+    {{item.overview}}
+    {{item.poster_path}}
+    {{item.genre_ids}}
+    {{item.first_air_date}}
+    </div>
+    {{watchedData.total_results}}
 </template>
+
+<script setup lang="ts">
+import {ref, onMounted} from 'vue'
+
+const watchedData = ref<Object | null>(null)
+const droppedData = ref(null)
+
+onMounted(() => {
+    watchedShowsData()
+    droppedShowsData()
+
+})
+
+async function watchedShowsData() {
+    watchedData.value = await $fetch('/api/watchedShows', {
+        method: 'GET'
+    });
+    console.log(typeof watchedData, 'huhu');
+}
+
+async function droppedShowsData() {
+    droppedData.value = await $fetch('/api/droppedShows', {
+        method: 'GET'
+    });
+}
+</script>
