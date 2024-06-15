cf460357aa9361736063e1bef341f4fc5aba6b4d
Vivien Wezel <v.wezel@dotsource.de>
2024-06-15 21:53:08 +0200

    add form input

--- vue components/SeriesHeader.vue
+++ vue components/SeriesHeader.vue
@@ -1,5 +1,10 @@
 <template>
-{{searchData}}
+<form action="/test">
+    <label for="search-query">Search Series:</label>
+    <input type="search" id="search-query" name="search-query">
+    <input type="submit">
+</form>
+
 </template>
 <script setup lang="ts">
 import {ref, onMounted} from 'vue'
